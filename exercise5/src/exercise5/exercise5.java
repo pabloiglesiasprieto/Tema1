d061631d6fbfe9f9907773c2d6d7f157997d8c75
pablo.iglesias <pablo.iglesias@106-13.info2>
2024-09-20 14:22:23 +0200

    Ejercicio5 terminado

---
+++ exercise5/src/exercise5/exercise5.java
@@ -0,0 +1,11 @@
+package exercise5;
+
+public class exercise5 {
+
+	public static void main(String[] args) {
+		System.out.println("Name: Pablo Iglesias Prieto");
+		System.out.println("Address: Mi casa");
+		System.out.println("Telephone Number: 722333642");
+	}
+
+}
