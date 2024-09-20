a54b3fb73a38778cefe36237d04d0b9afe34951c
pablo.iglesias <pablo.iglesias@106-13.info2>
2024-09-20 14:25:09 +0200

    Exercise7

---
+++ exercise7/src/exercise7/exercise7.java
@@ -0,0 +1,13 @@
+package exercise7;
+
+public class exercise7 {
+
+	public static void main(String[] args) {
+		System.out.println("**********************              *******************            ********************");
+		System.out.println("*                    *                       *                     *                  *\r\n"		+ "");		System.out.println("*                    *                       *                     *                  *\r\n"		+ "");		System.out.println("*                    *                       *                     *                  *\r\n"		+ "");		System.out.println("**********************                       *                     ********************\r\n"		+ "");		System.out.println("*                                            *                     *\r\n"		+ "");		System.out.println("*                                            *                     *\r\n"		+ "");		System.out.println("*                                            *                     *\r\n"		+ "");		System.out.println("*                                            *                     *\r\n"		+ "");		System.out.println("*                                   ********************           *");
+	}
+
+
+
+
+}
