db90a2e89db466af1a4cd042216583b9e8f35dc7
pablo.iglesias <pablo.iglesias@106-13.info2>
2024-09-20 13:44:29 +0200

    Ejercicio 1 terminado

---
+++ Exercise1/src/exercise1/exercise1.java
@@ -0,0 +1,9 @@
+package exercise1;
+
+public class exercise1 {
+	public static void main(String[] args) {
+		System.out.print("Hello, how are you? ");
+		System.out.println("Fine thanks.");
+
+	}
+}
