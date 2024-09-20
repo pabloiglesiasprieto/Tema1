95e55853001351e5b367be7a69d9246209497dd3
pablo.iglesias <pablo.iglesias@106-13.info2>
2024-09-20 14:12:16 +0200

    Ejercicio2

---
+++ exercise2/src/exercise2/exercise2.java
@@ -0,0 +1,11 @@
+package exercise2;
+
+public class exercise2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, how are you? ");
+		System.out.println("Fine thanks.");
+
+	}
+
+}
