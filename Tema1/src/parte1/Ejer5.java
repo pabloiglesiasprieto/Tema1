2a4f078ff4388f5e254747e18c26f1f0b15daae5
pablo.iglesias <pablo.iglesias@106-13.info2>
2024-09-23 13:44:43 +0200

    si

---
+++ Tema1/src/parte1/Ejer5.java
@@ -0,0 +1,20 @@
+package parte1;
+
+public class Ejer5 {
+	public static void main(String[] args) {
+		/* Diseñar una aplicación que calcule la longitud y el área de una
+		circunferencia. Para ello, el usuario debe introducir el radio, que puede
+		contener decimales. Usa Math.PI para tomar el valor de PI. (longitud = 2πr,
+		área=πr2)*/
+		
+		// Creamos el Scanner.
+		Scanner input = new Scanner(System.in);
+		// Declaramos la variable radio.
+		double radio;
+		// Pedimos al usuario que introduzca el radio.
+		System.out.println("Introduce el radio de la circunferencia");
+		
+		
+
+	}
+}
