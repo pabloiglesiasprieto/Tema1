e9bc6c80e98a4bb85b56566457cbd15779e8524b
pablo.iglesias <pablo.iglesias@106-13.info2>
2024-09-24 08:18:51 +0200

    finiquitao

--- Tema1/src/parte1/Ejer1.java
+++ Tema1/src/parte1/Ejer1.java
@@ -4,17 +4,18 @@ import java.util.Scanner;
 
 public class Ejer1 {
 	public static void main(String[] args) {
-		// Declara la variable numero
+		// Declaramos la variable numero.
 		int numero;
 		// Creamos Scanner
 		Scanner input = new Scanner(System.in);
-		// Pidamos al usuario que introduzca un numero
+		// Pidamos al usuario que introduzca un numero.
 		System.out.println("Introduce un numero");
 		/* Establecemos que el valor de la variable sea la siguiente entrada del
 		teclado.*/
 		numero = input.nextInt();
-		// Imprimimos en pantalla el resultado
+		// Imprimimos en pantalla el resultado.
 		System.out.println("El número que has elegido es " + numero);
+		// Cerramos el Scanner.
 		input.close();
 	}
 }
